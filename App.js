673d9c16fa484a08999d985e3cd20488a1062492
Lucas Campos <lucas.cgoncalves7@gmail.com>
2023-11-03 19:31:02 -0300

    configuração das imagens

--- App.js
+++ App.js
@@ -25,7 +25,7 @@ function HomeScreen({ navigation }) {
           <Image
             style={styles.image}
             source={{
-              uri: 'https://via.placeholder.com/150',
+              uri: 'https://static.riachuelo.com.br/RCHLO/12242365001/portrait/perfume-nina-ricci-feminino-eau-de-toilette-80ml-12242365_foto1_frontal.jpg',
             }}
           />
           <Text style={styles.text}>Produto 1 - R$10.00</Text>
@@ -34,7 +34,7 @@ function HomeScreen({ navigation }) {
           <Image
             style={styles.image}
             source={{
-              uri: 'https://via.placeholder.com/150',
+              uri: 'https://th.bing.com/th/id/R.904c5e742896eb657b98c20e88f87323?rik=xTUMxA15h8Hx6g&pid=ImgRaw&r=0',
             }}
           />
           <Text style={styles.text}>Produto 2 - R$15.00</Text>
@@ -45,7 +45,7 @@ function HomeScreen({ navigation }) {
           <Image
             style={styles.image}
             source={{
-              uri: 'https://via.placeholder.com/150',
+              uri: 'https://seuencanto.com.br/wp-content/uploads/2022/12/Perfume-Eudora-Feminino-Royal.jpg',
             }}
           />
           <Text style={styles.text}>Produto 3 - R$20.00</Text>
@@ -54,7 +54,7 @@ function HomeScreen({ navigation }) {
           <Image
             style={styles.image}
             source={{
-              uri: 'https://via.placeholder.com/150',
+              uri: 'https://http2.mlstatic.com/eudora-eau-de-parfum-75ml-indulgent-cream-250g-novo-frasco-D_NQ_NP_974890-MLB29843303273_042019-O.jpg',
             }}
           />
           <Text style={styles.text}>Produto 4 - R$25.00</Text>
